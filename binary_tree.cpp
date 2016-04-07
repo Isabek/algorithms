a1410d4f722a22d254225e73b575abfe17ce4e3b
Isabek <isabek2309@gmail.com>
2016-04-07 16:16:58 +0300

    Added binary tree. Added traversals to binary tree.

---
+++ binary_tree.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+
+
+struct TreeNode
+{
+	int value;
+
+	TreeNode *left;
+	TreeNode *right;
+
+	TreeNode(int value): value(value), left(NULL), right(NULL){}
+};
+
+void insert(TreeNode *&tree, int value){
+	if(tree == NULL){
+		tree = new TreeNode(value);
+		return;
+	}
+	else if(value < tree->value){
+		insert(tree->left, value);
+	}
+	else{
+		insert(tree->right, value);
+	}
+}
+
+void preOrder(TreeNode *tree){
+	if(tree != NULL){
+		printf("%d ", tree->value);
+		preOrder(tree->left);
+		preOrder(tree->right);
+	}
+}
+
+void inOrder(TreeNode *tree){
+	if(tree != NULL){
+		preOrder(tree->left);
+		printf("%d ", tree->value);
+		preOrder(tree->right);
+	}
+}
+
+int main()
+{
+	TreeNode *binaryTree = NULL;
+	int a[7] = {4, 2, 7, 1, 3, 6, 9};
+
+	for(int i = 0; i < 7; i ++){
+		insert(binaryTree, a[i]);
+	}
+
+	preOrder(binaryTree);
+	printf("\n");
+	inOrder(binaryTree);
+	printf("\n");
+	
+	return 0;
+}
